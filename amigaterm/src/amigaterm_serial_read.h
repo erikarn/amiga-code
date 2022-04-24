a77762b646f57b680267e3821231daacab4e281c
adrian chadd <adrian@freebsd.org>
2022-04-23 22:11:09 -0700

    [amigaterm] pull out the actual serial read routines that are useful

--- amigaterm/src/amigaterm_serial_read.h
+++ amigaterm/src/amigaterm_serial_read.h
@@ -1,6 +1,8 @@
 #ifndef	__AMIGATERM_SERIAL_READ_H__
 #define	__AMIGATERM_SERIAL_READ_H__
 
+extern	serial_retval_t readchar_sched(int schedule_next, int timeout_ms,
+	    unsigned char *ch);
 extern	serial_retval_t readchar_flush(int timeout_ms);
 extern	serial_retval_t readchar_buf(char *buf, int len);
 extern	serial_retval_t readchar(unsigned char *ch);
